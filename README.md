d813ece4808ba28fc1ef3a035a3a6f34a33e0f1b
aalemanyz67 <albertaleman67@gmail.com>
2023-10-07 23:31:48 -0500

    updated and finalized work-day-Scheduler

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Work-Day-Scheduler
+Name: Alberto Aleman 
+Github: https://github.com/aalemanyz67/Work-Day-Scheduler
+Website:  https://aalemanyz67.github.io/Work-Day-Scheduler/
+
+# Objective:
+In this challenge, I created a scheduler in which an individual can log and save events. The user can also see if the time is in past, present, and future.
+
+# How to use this application:
+Once webpage has been opened type your event at any hour and it will be saved.
+THe blocks will be color coded to signify the following. Block of time in grey means this event is in the past. Block of time in red means this event is in the present.
+Lastly, the block(s)
+ of time in green mean this event is in the future.
+# Technology and Resources:
+For this application I used the following resources.
+w3schools.com
+developer.mozilla.org
+youtube content creators:
+Brian Design
+GreatStack
+Web Cifar
+
+# Reflection:
+This challenge was difficult but fun. My understanding of JavaScript is increasing and I look forward to furthering my skills. This challenge showed me where I stand with the concepts learned in class and what areas I need to further study.. With the help of my tutor I was able to inccorporate many of the functions and abilites used in javascript. I will continue to learn from this activity and further my skills and understanding of javascripts, jquery, bootstrap and other applications.
--- assets/style.js
+++ assets/style.js
@@ -21,6 +21,21 @@ var storage = localStorage.getItem("hour-9")
 $("#hour-9").children(".description").val(storage)
 var storage = localStorage.getItem("hour-10")
 $("#hour-10").children(".description").val(storage)
+var storage = localStorage.getItem("hour-11")
+$("#hour-11").children(".description").val(storage)
+var storage = localStorage.getItem("hour-12")
+$("#hour-12").children(".description").val(storage)
+var storage = localStorage.getItem("hour-13")
+$("#hour-13").children(".description").val(storage)
+var storage = localStorage.getItem("hour-14")
+$("#hour-14").children(".description").val(storage)
+var storage = localStorage.getItem("hour-15")
+$("#hour-15").children(".description").val(storage)
+var storage = localStorage.getItem("hour-16")
+$("#hour-16").children(".description").val(storage)
+var storage = localStorage.getItem("hour-17")
+$("#hour-17").children(".description").val(storage)
+
 //})
 //saveBtn.addEventListener("click",function(event){
     //event.preventDefault();
@@ -30,10 +45,39 @@ function displayTime() {
     var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
     timeDisplayEl.text(rightNow);
   }
-$("description").each(function(index){
+$(".description").each(function(index){
     var hour = dayjs().hour()
+    var id = $(this).parent().attr("id")
+    id = id.split ("-")[1]
+    if (id < hour ) {
+        $(this).addClass('past')
 
+    }
+    if (id > hour ) {
+        $(this).addClass('future')
+
+    }
+    if (id = hour ) {
+        $(this).addClass('past')
+
+    }
 })
+$(".description").each(function(index){
+    var minute = dayjs().minute()
+    var id = $(this).parent().attr("id")
+    id = id.split ("-")[1]
+    if (id < minute ) {
+        $(this).addClass('past')
 
+    }
+    if (id > minute ) {
+        $(this).addClass('future')
+
+    }
+    if (id = minute ) {
+        $(this).addClass('past')
+
+    }
+})
 displayTime();
 setInterval(displayTime, 1000);
